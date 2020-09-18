9c391b32df5cfb25f898da7adb90dace9ffd3001
iamanh1990 <quocanh1990bach@gmail.com>
2020-09-18 16:38:42 +0300

    start remove expense

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -6,10 +6,12 @@ import {
 	editExpense,
 	removeExpense,
 	setExpenses,
-	startSetExpenses
+	startSetExpenses,
+	startRemoveExpense
 } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
+import { create } from 'react-test-renderer';
 
 const createMockStore = configureMockStore([ thunk ]);
 
@@ -28,6 +30,22 @@ test('Should setup remove expense action object', () => {
 	expect(action).toEqual({
 		type: 'REMOVE_EXPENSE',
 		id: '123abc'
+	});
+});
+
+test('should remove expense from firebase', (done) => {
+	const store = createMockStore({});
+	const id = expenses[2].id;
+	store.dispatch(startRemoveExpense({ id })).then(() => {
+		const actions = store.getActions();
+		expect(actions[0]).toEqual({
+			type: 'REMOVE_EXPENSE',
+			id
+		});
+		return database.ref(`expenses/${id}`).once('value').then((snapshot) => {
+			expect(snapshot.val()).toBeFalsy();
+			done();
+		});
 	});
 });
 
